75f82498070ad6e66e596b32365965561bb0e7ac
robinnnnnn <rlifshitz19@gmail.com>
2020-06-18 14:10:00 -0700

    Added Some Packages

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-
-const PORT = 3003;
+const ReviewModel = require('../database/db.js');
 
 const db = require('../database/db.js');
+
+const PORT = 3003;
 
 const app = express();
 
@@ -13,6 +14,19 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(bodyParser.json());
+
+// test this for return object type or length
+app.get('/reviews', (req, res) => {
+  ReviewModel.find((err, results) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(results);
+      res.send(results);
+    }
+  }).limit(10);
+
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
