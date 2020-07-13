16dbd27a0cfbdd2827a35688582bdbd533029ee2
mjrosenberg <mjr70@duke.edu>
2020-07-13 09:52:06 -0700

    testing routes

--- tests/getReviews.test.js
+++ tests/getReviews.test.js
@@ -4,18 +4,38 @@ const helpers = require('./helpers.js');
 
 describe('Server Endpoints', () => {
 
-  it('should return an array of lenght 10', () => {
-   expect.assertions(1);
-
-   return helpers.fetchReviews()
+  it('should get reviews by product id (number 28)', () => {
+   return helpers.fetchReviews(28)
     .then(data => {
-      expect(data.length).toBe(10);
+      expect(data.length).toBe(55);
+      expect(data[0].firstname).toBe('Fidel');
+      expect(data[0].lastname).toBe('Dicki');
     })
     .catch(err => err);
 
   });
+  it('should get reviews by product id (number 72190)', () => {
+    return helpers.fetchReviews(72190)
+    .then(data => {
+      expect(data.length).toBe(44);
+      expect(data[0].firstname).toBe('Olaf');
+      expect(data[0].lastname).toBe('Schultz');
+    })
+    .catch(err => err);
 
+  });
+  it('should get the first 1000 elements', () => {
+    return helpers.topOnePercent()
+    .then(data => {
+      expect(data.length).toBe(1000);
+      expect(data[0].firstname).toBe('Oliver');
+      expect(data[0].lastname).toBe('Cool');
+      expect(data[999].firstname).toBe('Hailie');
+      expect(data[999].lastname).toBe('Lind');
+    })
+    .catch(err => err);
 
+  });
 })
 
 
